25dd040ea45e08846058e062d1202ff6cc41f2ca
marcin <marcinzielinski1983@googlemail.com>
2021-03-15 21:44:38 +0000

    [add] - first commit

---
+++ src/main/java/pl/sda/maniek/homeworkitem/HomeworkitemApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.maniek.homeworkitem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkitemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkitemApplication.class, args);
+    }
+
+}
