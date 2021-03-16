38430a79e7bd11d4c26f61183d3b0d45b6303c44
marcin <marcinzielinski1983@googlemail.com>
2021-03-16 20:34:15 +0000

    [changes] - few changes in .gitignore

---
+++ src/test/java/pl/sda/maniek/homeworkitem/HomeworkitemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.maniek.homeworkitem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkitemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
